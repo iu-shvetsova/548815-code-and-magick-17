1bffcdafc0a196f3fd682b10e465410df541458a
iu-shvetsova <iu-shvetsova69@yandex.ru>
2019-06-10 10:52:42 +0300

    Добавлена ; 😳

--- js/setup.js
+++ js/setup.js
@@ -12,7 +12,7 @@ var wizards = [];
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(101, 137, 164)', 'rgb(146, 100, 161)', 'rgb(146, 100, 161)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green']
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var getRandomElement = function (elements) {
   return elements[Math.floor(Math.random() * elements.length)];
